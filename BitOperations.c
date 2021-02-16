5a63e33d2441ab3494e9faea71b2d7d2a905d16f
KennyThomas <G00359994@gmit.ie>
2021-02-16 18:37:47 +0000

    Left shift operator

--- BitOperations.c
+++ BitOperations.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 
+
+void bin_print(int i){
+    int j = (sizeof(int)*8)-1;  //num of bits in an integer
+
+
+    int k;
+
+    for(j--; j>=0; j--){
+        k = ((1 << j) & i) ? 1 : 0;
+        printf("%d",k);
+    }
+}
+
 int main(int argc, char *argv[]){
-    printf("%s\n","Hello World again");
+    int i = 241;
+    printf("Original:  ");
+    bin_print(i);
+    printf("\n");
+
+    for(int j = 0; j < 40; j++){
+        printf("%3d << %3d ", i , j);
+        bin_print(i<<j);
+        printf("\n");
+    }
+
 
     return 0;
 }
