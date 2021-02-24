ddfc0283c18237cfe8ba740a60e621c820e31d13
KennyThomas <G00359994@gmit.ie>
2021-02-24 15:30:22 +0000

    Attempt at ROTR

--- funcs.c
+++ funcs.c
@@ -3,7 +3,18 @@
 #define WORD uint32_t
 #define PF PRIX32
 
+void bin_print(unsigned int i){
+    int j = (sizeof(unsigned int)*8);  //num of bits in an integer
 
+    // Temporary value
+    int k;
+    //loop over the number of bits in i,  left to right
+    for(j--; j>=0; j--){
+        //Pick out the j^th bit of i 
+        k = ((1 << j) & i) ? 1 : 0;
+        printf("%d",k);
+    }
+}
 
 WORD Ch(WORD x , WORD y, WORD z){
     return (x & y)^(~ x & z);
@@ -16,6 +27,10 @@ WORD Maj(WORD x , WORD y, WORD z){
 
 }
 
+WORD ROTR(WORD x, int n){
+   return (x)=(x>>n) | (x<<x*16-n);
+
+}
 
 
 
@@ -28,9 +43,10 @@ int main(int argc, char *argv[]){
     
     WORD ch = Ch(x,y,z);
     WORD maj = Ch(x,y,z);
+    WORD rotr = ROTR(x,2);
 
     printf("Ch(%08" PF  ",%08"  PF  ",%08" PF ") = %08" PF "\n",x, y, z, ch);
     printf("Maj(%08" PF  ",%08"  PF  ",%08" PF ") = %08" PF "\n",x, y, z, maj);
-
+    printf("ROTR(%08" PF " = %08" PF "\n",y, rotr);
     return 0;
 }
