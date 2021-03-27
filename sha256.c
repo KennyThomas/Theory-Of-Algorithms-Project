5409d6e9f24d4842bd9cc00ef5d0a9979ca4dafa
KennyThomas <G00359994@gmit.ie>
2021-03-27 13:51:07 +0000

    Sha256 working

--- sha256.c
+++ sha256.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 #include <inttypes.h>
+#include <byteswap.h>
+
+const int _i = 1;
+#define is_lilendian() ((*(char*)&_i) != 0)
 
 #define WORD uint32_t
-#define PF PRIX32
-#define WLEN 32
+#define PF PRIx32
+
 #define BYTE uint8_t
 //Functions
-#define ROTR(x,n) (x>>n) | (x<<(WLEN-n))
-#define ROTL(x,n) (x<<n) | (x>>(WLEN-n))
+#define ROTR(x,n) ((x>>n) | (x<<((sizeof(x)*8)-n)))
+#define ROTL(x,n) ((x<<n) | (x>>((sizeof(x)*8)-n)))
+#define SHR(x,n) (x>>n)
 
-#define CH(x,y,z) (x & y)^(~ x & z)
-#define MAJ(x,y,z) (x & y) ^ (x & z) ^ (y & z)
-#define SHR(x,n) x>>n
+#define CH(x,y,z) ((x & y)^(~x & z))
+#define MAJ(x,y,z) ((x & y) ^ (x & z) ^ (y & z))
 
-#define SIG0(x) ROTR(x,2)^ROTR(x,13)^ROTR(x,22)
-#define SIG1(x) ROTR(x,6)^ROTR(x,11)^ROTR(x,25)
+#define SIG0(x) (ROTR(x,2)^ROTR(x,13)^ROTR(x,22))
+#define SIG1(x) (ROTR(x,6)^ROTR(x,11)^ROTR(x,25))
 
-#define Sig0(x) ROTR(x,7)^ROTR(x,18)^SHR(x,3)
-#define Sig1(x) ROTR(x,17)^ROTR(x,19)^SHR(x,10)
+#define Sig0(x) (ROTR(x,7)^ROTR(x,18)^SHR(x,3))
+#define Sig1(x) (ROTR(x,17)^ROTR(x,19)^SHR(x,10))
 
 
 union Block{
@@ -30,15 +34,25 @@ enum Status{
     READ, PAD, END
 };
 
-const WORD K[]={
-0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ce, 0xbef9a3f7, 0xc67178f2};
+const WORD K[] = {
+
+    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
+    0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+    0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
+    0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+    0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
+    0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
+    0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+    0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
+    0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
+    0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
+    0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
+    0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+};
 
 //5.3.4
 
@@ -57,9 +71,9 @@ int next_block(FILE *f, union Block *M, enum Status *S , uint64_t *nobits){
         nobytes = fread(M->bytes, 1 , 64 , f);
         //Update total bits read
         *nobits = *nobits + (8 * nobytes);
-        if(nobytes == 64){
-            return 1;
-        } else if(nobytes <= 56){
+
+        if(nobytes == 64){      
+        } else if(nobytes < 56){
             M->bytes[nobytes] = 0x80; // in bits: 10000000
 
             for(nobytes++; nobytes < 56;nobytes++){
@@ -67,7 +81,7 @@ int next_block(FILE *f, union Block *M, enum Status *S , uint64_t *nobits){
             }
 
             // Append length of original input
-            M->sixf[7] = *nobits;
+            M->sixf[7] = (is_lilendian() ? bswap_64(*nobits)  : *nobits);
             //say this is the last block
             *S = END;
         }else{
@@ -85,9 +99,14 @@ int next_block(FILE *f, union Block *M, enum Status *S , uint64_t *nobits){
             M->bytes[nobytes] = 0x00; // in bits: 00000000
         }
         //Change status to END
-        M->sixf[7] = *nobits;
+         M->sixf[7] = (is_lilendian() ? bswap_64(*nobits) : *nobits);
         *S = END;
     }
+
+    if(is_lilendian())
+        for(int i = 0; i < 16; i++)
+            M->words[i] = bswap_32(M->words[i]);
+
 
     return 1;
 }
@@ -116,15 +135,16 @@ int next_hash(union Block *M , WORD H[]){
 
     //Section 6.2.2 part 3
     for(t=0; t < 64; t++){
-        T1 = SIG1(e) + CH(e,f,g) + K[t] + W[t];
+        T1 = h + SIG1(e) + CH(e,f,g) + K[t] + W[t];
         T2 = SIG0(a) + MAJ(a,b,c);
-        h = g; g = f; f = e; e = d + T1; d = c; c = b; b = a; a = T1 = T2;
+        h = g; g = f; f = e; e = d + T1; d = c; c = b; b = a; a = T1 + T2;
     }
 
     //Section 6.2.2 part 4
 
     H[0] = a + H[0]; H[1] = b + H[1]; H[2] = c + H[2]; H[3] = d + H[3]; 
     H[4] = e + H[4]; H[5] = f + H[5]; H[6] = g + H[6]; H[7] = h + H[7];
+    return 0;
 }
 
 int sha256(FILE *f, WORD H[]){
@@ -157,10 +177,9 @@ int main(int argsc , char *argv[]){
     sha256(f, H);
     //print hash
     for (int i = 0; i < 8; i++){
-        printf("%08"PF, H[i]);
-    printf("\n");
+        printf("%08" PF, H[i]);
     }
-
+    printf("\n");
     //close file
     fclose(f);
 
