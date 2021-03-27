c012797a80f5850142f63db34a08d3c8bda7cbde
KennyThomas <G00359994@gmit.ie>
2021-03-27 12:43:13 +0000

    Calculating the hash for SHA256

---
+++ sha256.c
@@ -0,0 +1,168 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+#define WORD uint32_t
+#define PF PRIX32
+#define WLEN 32
+#define BYTE uint8_t
+//Functions
+#define ROTR(x,n) (x>>n) | (x<<(WLEN-n))
+#define ROTL(x,n) (x<<n) | (x>>(WLEN-n))
+
+#define CH(x,y,z) (x & y)^(~ x & z)
+#define MAJ(x,y,z) (x & y) ^ (x & z) ^ (y & z)
+#define SHR(x,n) x>>n
+
+#define SIG0(x) ROTR(x,2)^ROTR(x,13)^ROTR(x,22)
+#define SIG1(x) ROTR(x,6)^ROTR(x,11)^ROTR(x,25)
+
+#define Sig0(x) ROTR(x,7)^ROTR(x,18)^SHR(x,3)
+#define Sig1(x) ROTR(x,17)^ROTR(x,19)^SHR(x,10)
+
+
+union Block{
+    BYTE bytes[64];
+    WORD words[16];
+    uint64_t sixf[8];
+};
+
+enum Status{
+    READ, PAD, END
+};
+
+const WORD K[]={
+0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ce, 0xbef9a3f7, 0xc67178f2};
+
+//5.3.4
+
+
+
+int next_block(FILE *f, union Block *M, enum Status *S , uint64_t *nobits){
+    
+    //no. bytes read
+    size_t nobytes;
+
+    if(*S == END){
+        return 0;
+        }
+        else if(*S == READ){
+        //try to read 64 bytes
+        nobytes = fread(M->bytes, 1 , 64 , f);
+        //Update total bits read
+        *nobits = *nobits + (8 * nobytes);
+        if(nobytes == 64){
+            return 1;
+        } else if(nobytes <= 56){
+            M->bytes[nobytes] = 0x80; // in bits: 10000000
+
+            for(nobytes++; nobytes < 56;nobytes++){
+                M->bytes[nobytes] = 0x00; // in bits: 00000000
+            }
+
+            // Append length of original input
+            M->sixf[7] = *nobits;
+            //say this is the last block
+            *S = END;
+        }else{
+            M->bytes[nobytes] = 0x80;
+
+            for(nobytes++; nobytes < 64; nobytes++){
+                M->bytes[nobytes] = 0x00; // in bits: 00000000
+            }
+            //Change status to PAD
+            *S = PAD;
+        }
+
+    }else if (*S == PAD){
+        for (nobytes = 0; nobytes <56; nobytes++){
+            M->bytes[nobytes] = 0x00; // in bits: 00000000
+        }
+        //Change status to END
+        M->sixf[7] = *nobits;
+        *S = END;
+    }
+
+    return 1;
+}
+
+
+
+int next_hash(union Block *M , WORD H[]){
+   
+    WORD W[64];
+    int t;
+
+    WORD a, b, c, d, e, f, g, h , T1 , T2;
+    
+   //Section 6.2.2 part 1
+    for(t = 0; t < 16; t++)
+        W[t] = M->words[t];
+    for (t = 16; t < 64; t++ )
+        W[t] = Sig1(W[t-2]) + W[t-7] + Sig0(W[t-15]) + W[t-16];
+
+
+
+    //Section 6.2.2 part 2
+
+    a = H[0]; b = H[1]; c = H[2]; d = H[3]; 
+    e = H[4]; f = H[5]; g = H[6]; h = H[7];
+
+    //Section 6.2.2 part 3
+    for(t=0; t < 64; t++){
+        T1 = SIG1(e) + CH(e,f,g) + K[t] + W[t];
+        T2 = SIG0(a) + MAJ(a,b,c);
+        h = g; g = f; f = e; e = d + T1; d = c; c = b; b = a; a = T1 = T2;
+    }
+
+    //Section 6.2.2 part 4
+
+    H[0] = a + H[0]; H[1] = b + H[1]; H[2] = c + H[2]; H[3] = d + H[3]; 
+    H[4] = e + H[4]; H[5] = f + H[5]; H[6] = g + H[6]; H[7] = h + H[7];
+}
+
+int sha256(FILE *f, WORD H[]){
+
+    union Block M;
+
+    //Total num of bytes read
+    uint64_t nobits = 0;
+
+    // Curren Status
+    enum Status S = READ;
+
+    while(next_block(f, &M, &S, &nobits)){
+     next_hash(&M,H);
+    }
+    return 0;
+}
+
+
+int main(int argsc , char *argv[]){
+    //5.3.4
+    WORD H[]={  0x6a09e667 , 0xbb67ae85 , 0x3c6ef372 , 0xa54ff53a , 
+                0x510e527f , 0x9b05688c , 0x1f83d9ab , 0x5be0cd19
+        };
+    //File pointer
+    FILE *f;
+    //open file from command line
+    f = fopen(argv[1], "r");
+
+    sha256(f, H);
+    //print hash
+    for (int i = 0; i < 8; i++){
+        printf("%08"PF, H[i]);
+    printf("\n");
+    }
+
+    //close file
+    fclose(f);
+
+    return 0;
+}
